670a6d7d3ffb6559055e3fa21d2496028a78a698
Yasser Carreón <yacasia@gmail.com>
2018-07-26 19:35:39 -0500

    Add params to routes

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.send('Hello World!!!');
+});
+
+app.get(/big.*/, function(req, res, next) {
+    console.log('BIG USER ACCESS');
+    next();
+});
+
+app.get(/.*dog.*/, function(req, res, next) {
+    console.log('DOGS GO WOOF');
+    next();
+});
+
+app.get('/:username', function (req, res) {
+    var username = req.params.username;
+    res.send(username);
 });
 
 var server = app.listen(3000, function() {
